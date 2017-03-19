8f9a41369c6465633945ff57546dc5d7672ffd8b
Dale Hudson <hudsonn90@gmail.com>
2017-03-19 21:16:29 +0000

    [Classes] Added latlon interface

---
+++ classes/interface/geolocation/latlon.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FuelGeoLocation;
+
+interface Interface_GeoLocation_LatLon
+{
+	/**
+	 * @return mixed
+	 */
+	public function get_latitude();
+
+	/**
+	 * @return mixed
+	 */
+	public function get_longitude();
+}
