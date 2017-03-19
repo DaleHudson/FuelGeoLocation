75def21ab90654c9b55973dfbc153276e2fc88ff
Dale Hudson <hudsonn90@gmail.com>
2017-03-19 22:00:40 +0000

    [Tests] Added initial tests

---
+++ tests/classes/testpostcodeio.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace FuelGeoLocation;
+
+require_once __DIR__ . '/../fuelgeolocationtestcase.php';
+require_once __DIR__ . '/../../classes/postcodeio.php';
+
+class Test_PostcodeIO extends FuelGeoLocationTestCase
+{
+	/**
+	 * @group Postcode
+	 */
+	public function test_successful_postcode_lookup()
+	{
+		$postcodeIO = new PostcodeIO();
+		$result = $postcodeIO->lookup($this->postcode);
+
+		$this->assertInstanceOf('stdClass', $result);
+		$this->assertInternalType('object', $result);
+		$this->assertObjectHasAttribute('result', $result);
+		$this->assertEquals(200, $result->status);
+
+		// Assertions for result->result object
+		$this->assertObjectHasAttribute('postcode', $result->result);
+		$this->assertObjectHasAttribute('longitude', $result->result);
+		$this->assertObjectHasAttribute('latitude', $result->result);
+	}
+
+	/**
+	 * @group Postcode
+	 */
+	public function test_successful_postcode_valid_lookup()
+	{
+		$postcodeIO = new PostcodeIO();
+		$result = $postcodeIO->valid_lookup($this->postcode);
+
+		$this->assertInstanceOf('stdClass', $result);
+		$this->assertInternalType('object', $result);
+		$this->assertObjectHasAttribute('result', $result);
+		$this->assertEquals(200, $result->status);
+		$this->assertTrue($result->result);
+	}
+
+	/**
+	 * @group Postcode
+	 *
+	 * @expectedException RequestStatusException
+	 */
+	public function test_unsuccessful_postcode_lookup()
+	{
+		$postcodeIO = new PostcodeIO();
+		$postcodeIO->lookup($this->invalid_postcode);
+	}
+
+	/**
+	 * @group Postcode
+	 */
+	public function test_unsuccessful_postcode_valid_lookup()
+	{
+		$postcodeIO = new PostcodeIO();
+		$result = $postcodeIO->valid_lookup($this->invalid_postcode);
+
+		$this->assertInstanceOf('stdClass', $result);
+		$this->assertInternalType('object', $result);
+		$this->assertObjectHasAttribute('result', $result);
+		$this->assertEquals(200, $result->status);
+		$this->assertFalse($result->result);
+	}
+}
---
+++ tests/fuelgeolocationtestcase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace FuelGeoLocation;
+
+require_once __DIR__ . '/../../../vendor/fzaninotto/faker/src/autoload.php';
+
+use Fuel\Core\TestCase;
+
+abstract class FuelGeoLocationTestCase extends TestCase
+{
+	/**
+	 * @var string
+	 */
+	protected $postcode = "B77 1JR";
+
+	/**
+	 * @var string
+	 */
+	protected $invalid_postcode = "CV215 6GHT";
+
+	/**
+	 * @var object
+	 */
+	protected $faker;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		\Migrate::latest('FuelGeoLocation', 'package');
+
+		\DBUtil::truncate_table('location');
+
+		// Set up faker
+		$this->faker = \Faker\Factory::create("en_GB");
+	}
+
+	public function tearDown()
+	{
+		parent::tearDown();
+
+		unset($this->faker);
+	}
+}
