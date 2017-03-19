9f4434ebd56a36c16b79d97cb9662bb442372b41
Dale Hudson <hudsonn90@gmail.com>
2017-03-19 21:17:05 +0000

    [Bootstrap] Added distance class and latlon interface to autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,7 @@
 
 \Autoloader::add_classes(array(
 	'FuelGeoLocation\\PostcodeIO' => __DIR__ . '/classes/postcodeio.php',
+	'FuelGeoLocation\\Distance' => __DIR__ . '/classes/distance.php',
 	'FuelGeoLocation\\Model_Location' => __DIR__ . '/classes/model/location.php',
+	'FuelGeoLocation\\Interface_GeoLocation_LatLon' => __DIR__ . '/classes/interface/geolocation/latlon.php',
 ));
