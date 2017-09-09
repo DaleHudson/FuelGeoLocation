a14f3ab6b2d0900d042188309c2f2839bb357606
Dale Hudson <hudsonn90@gmail.com>
2017-09-09 14:58:04 +0100

    [Model] Updated google provider save_geocode_data signiture

--- classes/model/provider/google.php
+++ classes/model/provider/google.php
@@ -34,14 +34,14 @@ class Model_Provider_Google extends Model_GeoCode
 	/**
 	 * Save the retrieved location data
 	 *
-	 * @param object|array $geocode_data
+	 * @param array $geocode_data
 	 * @param string $search_term The search term to save
 	 *
 	 * @return bool
 	 *
 	 * @throws \Exception
 	 */
-	public function save_geocode_data($geocode_data, $search_term)
+	public function save_geocode_data(array $geocode_data, $search_term)
 	{
 		$this->set_search_term($search_term);
 		$this->set_latitude($geocode_data[0]['geometry']['location']['lat']);
@@ -50,17 +50,5 @@ class Model_Provider_Google extends Model_GeoCode
 		$this->set_provider("Google");
 
 		return $this->save();
-	}
-
-	/**
-	 * Set the search results
-	 *
-	 * json encode the results for storing in DB
-	 *
-	 * @param array $results
-	 */
-	public function set_search_result(array $results)
-	{
-		$this->search_result = json_encode($results);
 	}
 }
